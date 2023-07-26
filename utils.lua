6d28adc2c2b52e47b9a8deb82e8cd9c29fd93b14
spags <jamestspagnola@gmail.com>
2023-07-25 20:06:49 -0700

    Fix missing function.

--- utils.lua
+++ utils.lua
@@ -163,3 +163,14 @@ function ICT:printKeys(t)
         print(k)
     end
 end
+
+function ICT:DisplayTime(time)
+    if not time then
+        return ""
+    end
+    local days = math.floor(time / 86400)
+    local hours = math.floor(time % 86400 / 3600)
+    local minutes = math.floor(time % 3600 / 60)
+    local seconds = math.floor(time % 60)
+    return string.format("%d:%02d:%02d:%02d", days, hours, minutes, seconds)
+end
